c838d9917cf98ed9a18a140a388da5f069e395d4
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2021-10-01 14:36:39 +0000

    installed packages

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello world")
+import requests
+from bs4 import BeautifulSoup
+
+indeed_result = requests.get("https://www.indeed.com/jobs?as_and=python&as_phr&as_any&as_not&as_ttl&as_cmp&jt=all&st&salary&radius=25&l=Austin%2C%20TX&fromage=any&limit=50&sort&psf=advsrch&from=advancedsearch&vjk=f2e0073c7a5ec089")
+
+print(indeed_result)
