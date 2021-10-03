28d1b87bb1b9d144cdb0f370dcd9e2fdcb1ddb4f
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2021-10-03 14:08:37 +0000

    another commit...

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-indeed_result = requests.get("https://www.indeed.com/jobs?as_and=python&as_phr&as_any&as_not&as_ttl&as_cmp&jt=all&st&salary&radius=25&l=Austin%2C%20TX&fromage=any&limit=50&sort&psf=advsrch&from=advancedsearch&vjk=f2e0073c7a5ec089")
-
-# print(indeed_result.text)
-indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
-# print(indeed_soup.prettify())
-
-pagination = indeed_soup.find("div", {"class":"pagination"})
-# print(pagination)
-
-pages = pagination.find_all('a')
-# print(pages)
-
-for page in pages:
-  print(page.find("span"))
-
-
+last_indeed_pages = extract_indeed_pages()
+extract_indeed_jobs(last_indeed_pages)
