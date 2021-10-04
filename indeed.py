792d88075ec61a0d02ca2f528c8e4e487d70a9d1
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2021-10-04 10:22:58 +0000

    style

--- indeed.py
+++ indeed.py
@@ -19,28 +19,34 @@ def extract_indeed_pages():
 
   return max_page
 
+def extract_job_info(result):
+
+  job_title = result.find("td", {"class":"resultContent"}).find("div", {"class":"heading4"}).find_all("span")
+  if len(job_title) > 1:
+    job_title = job_title[1].string
+  else:
+    job_title = job_title[0].string
+
+  NO_COMPANY_NAME = None
+  find_company = result.find("div",{"class":"heading6"}).find("span",{"class":"companyName"}).find("a")
+  if type(find_company) == type(NO_COMPANY_NAME):
+    company_name = str(NO_COMPANY_NAME)
+  else:
+    company_name = find_company.string
+  
+  return {"job: " + job_title + " , company: " + company_name}
+
+
 def extract_indeed_jobs(last_page):
+  jobs = []
   # for page in range(last_page):
   result = requests.get(f"{URL}&start={0*LIMIT}")
   soup = BeautifulSoup(result.text, 'html.parser')
   results = soup.find_all("a", {"class":"fs-unmask"})
-  index = 1
   for result in results:
-    job_title = result.find("td", {"class":"resultContent"}).find("div", {"class":"heading4"}).find_all("span")
-    if len(job_title) > 1:
-      job_title = job_title[1].string
-    else:
-      job_title = job_title[0].string
-
-    NO_COMPANY_NAME = None
-    find_company = result.find("div",{"class":"heading6"}).find("span",{"class":"companyName"}).find("a")
-    if type(find_company) == type(NO_COMPANY_NAME):
-      company_name = str(NO_COMPANY_NAME)
-    else:
-      company_name = find_company.string
-    
-    print("#" + str(index) + " - job: " + job_title + " / company: " + company_name)
-    index += 1
+    job = extract_job_info(result)
+    jobs.append(job)
+    print(job)
     
 
 
