7687babc594e420441933c9f4db65de7e25ffee9
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2021-10-05 14:35:11 +0000

    using flask

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from indeed import extract_indeed_pages, extract_indeed_jobs
-from save import save_to_file
+# from indeed import extract_indeed_pages, extract_indeed_jobs
+# from save import save_to_file
+from flask import Flask
 
-last_indeed_pages = extract_indeed_pages()
-indeed_jobs = extract_indeed_jobs(last_indeed_pages)
-save_to_file(indeed_jobs)
+# last_indeed_pages = extract_indeed_pages()
+# indeed_jobs = extract_indeed_jobs(last_indeed_pages)
+# save_to_file(indeed_jobs)
+
+app = Flask("SuperScrapper")
+
+@app.route("/")
+def home():
+    return "Hello! Welcome to mi casa!"
+
+@app.route("/contact")
+def contact():
+    return "Contact me!"
+
+app.run(host="0.0.0.0")
+
+
+
