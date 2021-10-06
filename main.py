3c953afb9605182800684de3d130e6fba9f4c66f
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2021-10-06 13:48:49 +0000

    render template = calls existing html templates via name attribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # from indeed import extract_indeed_pages, extract_indeed_jobs
 # from save import save_to_file
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # last_indeed_pages = extract_indeed_pages()
 # indeed_jobs = extract_indeed_jobs(last_indeed_pages)
@@ -12,7 +12,14 @@ app = Flask("SuperScrapper")
 def home():
     return render_template("home.html")
 
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    return render_template("report.html", searchingBy=word)
+
 app.run(host="0.0.0.0")
+
+# start from 4.4
 
 
 
